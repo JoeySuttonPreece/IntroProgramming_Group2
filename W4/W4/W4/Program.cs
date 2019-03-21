d15811d5a3430a371782d48cb68e8c349b40c8b0
JoeySuttonPreece <48079206+JoeySuttonPreece@users.noreply.github.com>
2019-03-21 16:47:27 +1100

    T1 complete hopefully

--- W4/W4/W4/Program.cs
+++ W4/W4/W4/Program.cs
@@ -14,12 +14,65 @@ namespace W4
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
+//			T1_1();
+			T1_2();
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		public static int GetNum() {
+			int num = 0;
+			
+			while (!Int32.TryParse(Console.ReadLine(), out num)) {
+				Console.WriteLine("Please enter a number!");
+			}
+			
+			return num;
+		}
+		
+		public static void T1_1() 
+		{
+			string input = Console.ReadLine();
+			
+			string output = "";
+			
+			int counter = 0;
+			
+			while (counter < 10) {
+				output += input;
+				counter++;
+			}
+			
+			counter = 0;
+			
+			while (counter < 10) {
+				Console.WriteLine(output);
+				counter++;
+			}
+		}
+		
+		public static void T1_2() 
+		{
+			string input = Console.ReadLine();
+			
+			int amount = GetNum();
+			
+			string output = "";
+			
+			int counter = 0;
+			
+			while (counter < amount) {
+				output += input;
+				counter++;
+			}
+			
+			counter = 0;
+			
+			while (counter < amount) {
+				Console.WriteLine(output);
+				counter++;
+			}
 		}
 	}
 }
