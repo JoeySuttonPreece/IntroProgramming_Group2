17220e61eec47fe3ca43afa4159e12e226d2ed99
Joey Sutton Preece <joeysuttonpreece@gmail.com>
2019-06-06 14:57:27 +1000

    restructure

---
+++ Week5/W5/W5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DuoVia;
+
+namespace W5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string old;
+            string current;
+
+            while (true)
+            {
+                current = Console.ReadLine();
+                double score = DuoVia.FuzzyStrings.DiceCoefficientExtensions.DiceCoefficient(Console.ReadLine(), Console.ReadLine());
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+       
+}
